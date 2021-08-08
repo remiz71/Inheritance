083125d2f160f645904043e2cf51edcc70c0e2ec
Remi_wr91@itstepru.onmicrosoft.com <Remi_wr91@itstepru.onmicrosoft.com>
2021-08-08 10:00:47 +0300

    improving , but i dont understand how to count total salary for otdel xD

--- Work_personell/main.cpp
+++ Work_personell/main.cpp
@@ -8,7 +8,6 @@ class Employee
     string last_name;
     string first_name;
     string job;
-    string salary_type;
 public:
     const string& get_last_name() const
     {
@@ -21,9 +20,6 @@ public:
     const string& get_job() const
     {
         return job;
-    }const string& get_type() const
-    {
-        return salary_type;
     }
     void set_last_name(const string& last_name)
     {
@@ -37,20 +33,16 @@ public:
     {
         this->job = job;
     }
-    void set_type(const string& salary_type)
-    {
-        this->salary_type = salary_type;
-    }
+
 
 
     /////////////////// Constructors
 
-    Employee(const string& last_name, const string& first_name, const string& job, const string& salary_type)
+    Employee(const string& last_name, const string& first_name, const string& job)
     {
         set_last_name(last_name);
         set_first_name(first_name);
         set_job(job);
-        set_type(salary_type);
         cout << "E_Constructor: \t" << this << endl;
 
     }
@@ -70,24 +62,34 @@ public:
 class Hourly : public Employee
 {
     double salary;
+    double work_hours;
 public:
     double get_salary() const
     {
         return salary;
     }
+    double get_hours() const
+    {
+        return work_hours;
+    }
     void set_salary(double salary)
     {
         this->salary = salary;
+    }
+    void set_hours(double work_hours)
+    {
+        this->work_hours = work_hours;
     }
 
     /////////////////// Constructors
 
     Hourly
     (const string& last_name, const string& first_name,
-        const string& job, const string& salary_type, double salary
-    ) :Employee(last_name, first_name, job, salary_type)
+        const string& job,double work_hours, double salary
+    ) :Employee(last_name, first_name, job)
     {
         set_salary(salary);
+        set_hours(work_hours);
         cout << "H_Constructor: \t" << this << endl;
     }
     ~Hourly()
@@ -100,11 +102,20 @@ public:
     {
         Employee::print();
         cout << "Заработная плата " << salary << " руб. в час" << endl;
+        cout << "Отработано " << work_hours << " часов" << endl;
+    }
+    
+    double count_salary(int salary, double work_hours) const
+    {
+        double total_salary;
+        total_salary = get_salary() * get_hours();
+        return total_salary;
     }
 };
 class Monthly :public Employee
     {
         double salary;
+        int work_days;
     public:
         double get_salary() const
         {
@@ -113,14 +124,22 @@ class Monthly :public Employee
         void set_salary(double salary)
         {
             this->salary = salary;
+        }int get_days() const
+        {
+            return work_days;
+        }
+        void set_days(int work_days)
+        {
+            this->work_days = work_days;
         }
         //Constructors:
         Monthly
             (const string& last_name, const string& first_name,
-                const string& job, const string& salary_type, double salary
-            ) :Employee(last_name, first_name, job, salary_type)
+                const string& job,int work_days, double salary
+            ) :Employee(last_name, first_name, job)
         {
             set_salary(salary);
+            set_days(work_days);
             cout << "M_Constructor: \t" << this << endl;
         }
         ~Monthly()
@@ -132,6 +151,11 @@ class Monthly :public Employee
         {
             Employee::print();
             cout << "Заработная плата " << salary << " руб. в месяц" << endl;
+            cout << "Отработано дней: " << work_days << endl;
+        }
+        double count_salary(int salary,double work_days)const
+        {
+            return (get_salary() / 31) * get_days();
         }
 
     };
@@ -140,15 +164,20 @@ int main()
     setlocale(LC_ALL, "");
     Employee* group[] =
     {
-        new Monthly("Парикожа", "Кирилл" , "Инженер линии","помесячно", 53000),
-        new Hourly("Ремизов", "Дмитрий" , "Младший инженер линии", "почасово", 200 ),
-        new Hourly("Фролов", "Денис", "начальник участка","почасово",350),
-        new Monthly("Лейко", "Дарья", "Руководитель инженерной группы", "помесячно", 65000),
-        new Hourly("Hu", "Xiaoliang" , "Head of paintshop" , "почасово", 500)
+        new Monthly("Парикожа", "Кирилл" , "Инженер линии", 22, 53000),
+        new Hourly("Ремизов", "Дмитрий" , "Младший инженер линии",158.5, 200 ),
+        new Hourly("Фролов", "Денис", "начальник участка",50.7,350),
+        new Monthly("Лейко", "Дарья", "Руководитель инженерной группы",15, 65000),
+        new Hourly("Hu", "Xiaoliang" , "Head of paintshop" ,700, 500)
     };
     for (int i = 0; i < sizeof(group) / sizeof(Employee*); i++)
     {
         group[i]->print();
+        cout << delim << endl;
+    } 
+    for (int i = 0; i < sizeof(group) / sizeof(Employee*); i++)
+    {
+        group[i];
         cout << delim << endl;
     }
     
